0e00ba4e62a2bbe2587b5bff7a67d1028f8d480f
Кожевникова Светлана <70895760+Fotonchik@users.noreply.github.com>
2020-11-03 12:35:56 +0300

    Кнопка с выводящимся сообщением
    Exemple 4 MainWnd Class

--- Source.cpp
+++ Source.cpp
@@ -1,117 +1,55 @@
 #include<Windows.h>
-#define DIVISIONS 5
-#define MoveTo(hdc,x,y) MovetoEx(hdc,x,y,NULL)
 
-LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-LRESULT CALLBACK ChildWndProc(HWND, UINT, WPARAM, LPARAM);
-char szChildClass[] = "Checker3_Child";
-HINSTANCE hInst;
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-	LPSTR szCmdLine, int iCmdShow) 
+LRESULT CALLBACK MainWinProc(HWND, UINT, WPARAM, LPARAM);
+#define ID_MYBUTTON 1
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE,
+	LPSTR , int ss)
 {
-	static char szAppName[] = "Checker3";
-	HWND hwnd;
+	
+	WNDCLASS ws;
+	ws.style = 0;
+	ws.lpfnWndProc = MainWinProc;
+	ws.cbClsExtra = 0;
+	ws.cbWndExtra = 0;
+	ws.hInstance = hInst;
+	ws.hIcon = NULL;
+	ws.hCursor = NULL;
+	ws.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+	ws.lpszMenuName = NULL;
+	ws.lpszClassName = "Exemple 4 MainWnd Class";
+	if(!RegisterClass(&ws)) return FALSE;
+	HWND hMainWnd = CreateWindow("Exemple 4 MainWnd Class", "EXAMPLE 4", WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+		NULL, NULL, hInst, NULL);
+	if(!hMainWnd) return FALSE;
+	ShowWindow(hMainWnd, ss);
+	UpdateWindow(hMainWnd);
 	MSG msg;
-	WNDCLASSEX wndclass;
-	wndclass.cbSize = sizeof(wndclass);
-	wndclass.style = CS_HREDRAW | CS_VREDRAW;
-	wndclass.lpfnWndProc = WndProc;
-	wndclass.cbClsExtra = 0;
-	wndclass.cbWndExtra = 0;
-	wndclass.hInstance = hInstance;
-	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
-	wndclass.lpszMenuName = NULL;
-	wndclass.lpszClassName = szAppName;
-	wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
-	RegisterClassEx(&wndclass);
-	hInst = hInstance;
-	hwnd = CreateWindow(szAppName, "Checker3 Demo",
-		WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
-		NULL, NULL, hInstance, NULL);
-
-	ShowWindow(hwnd, iCmdShow);
-	UpdateWindow(hwnd);
-	while (GetMessage(&msg, NULL, 0, 0)) {
+	while (GetMessage(&msg,NULL,0,0))
+	{
 		TranslateMessage(&msg);
-		DispatchMessageW(&msg);
+		DispatchMessage(&msg);
 	}
 	return msg.wParam;
 }
-LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam) {
-	static HWND hwndChild[DIVISIONS][DIVISIONS];
-	int cxBlock, cyBlock,x,y;
+LRESULT CALLBACK MainWinProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 	
-	switch (iMsg) {
+	switch (msg) {
 	case WM_CREATE:
-		for ( x = 0; x < DIVISIONS; x++)
-		
-			for ( y = 0; y < DIVISIONS; y++)
-			{
-				hwndChild[x][y] = CreateWindow(szChildClass, NULL,
-					WS_CHILDWINDOW | WS_VISIBLE,
-					0, 0, 0, 0,
-					hwnd, (HMENU)(Y << 8 | X),
-					(HINSTANCE)GetWindowLong(hwnd, GWL_HINSTANCE),
-					NULL);
-			}
-		
+		CreateWindow("button", "My button", WS_CHILD | BS_PUSHBUTTON | WS_VISIBLE,
+			5, 5, 100, 20, hwnd, (HMENU)ID_MYBUTTON, NULL, NULL);
+
 		return 0;
-	case WM_SIZE:
-		cxBlock = LOWORD(lParam)/ DIVISIONS;
-		cyBlock = HIWORD(lParam)/ DIVISIONS;
-		for (x = 0; x < DIVISIONS; x++)
-			for (y = 0; y < DIVISIONS; y++)
-				MoveWindow(hwndChild[x][y],
-					x * cxBlock, y * cyBlock,
-					cxBlock, cyBlock, TRUE);
-		return 0;
-	case WM_LBUTTONDOWN:
-		MessageBeep(0);
+	case WM_COMMAND:
+		if ((HIWORD(wParam) == 0) && (LOWORD(wParam) == ID_MYBUTTON))
+			MessageBox(hwnd, "You pressed my button", "MessageBox", MB_OK | MB_ICONWARNING);
+
 		return 0;
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		return 0;
 	}
-	return DefWindowProc(hwnd, iMsg, wParam, lParam);
+	return DefWindowProc(hwnd, msg, wParam, lParam);
 
 }
-
-LRESULT CALLBACK ChildWndProc(HWND hwnd, UINT iMsg,
-	WPARAM wParam, LPARAM lParam) 
-{
-	HDC hdc;
-	PAINTSTRUCT ps;
-	RECT rect;
-
-	switch (iMsg) {
-	case WM_CREATE:
-		SetWindowWord(hwnd, 0, 0);
-		return 0;
-	case WM_LBUTTONDOWN:
-		SetWindowWord(hwnd, 0, 1 ^ GetWindowWord(hwnd, 0));
-		InvalidateRect(hwnd, NULL, FALSE);
-		return 0;
-
-	case WM_PAINT:
-		hdc = BeginPaint(hwnd, &ps);
-		GetClientRect(hwnd, &rect);
-		Rectangle(hdc, 0, 0, rect.right, rect.bottom);
-		if (GetWindowWord(hwnd, 0))
-		{
-			//MoveTo(hdc, 0, 0);
-			LineTo(hdc, rect.right, rect.bottom);
-
-		}
-		EndPaint(hwnd, &ps);
-		return 0;
-	}
-	return DefWindowProc(hwnd, iMsg, wParam, lParam);
-
-}
-
