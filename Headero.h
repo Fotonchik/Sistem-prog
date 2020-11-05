914a77082dc88138937ca2987a2c9479749a5cc9
Кожевникова Светлана <70895760+Fotonchik@users.noreply.github.com>
2020-11-05 11:43:51 +0300

    Меню

---
+++ Headero.h
@@ -0,0 +1,10 @@
+#define IDM_NEW 100
+#define IDM_CUT 101
+#define IDM_SAVE 102
+#define IDM_SAVEAS 103
+#define IDM_PASTE 104
+#define IDM_DEL 105
+#define IDM_UNDO 106
+#define IDM_COPY 107
+#define IDM_EXIT 108
+
